68beb12475858072d2d41bf8a4477a8a55fa6ef5
Utkarsh110-uz <utkarsh66488@gmail.com>
2025-08-25 09:14:51 +0530

    Changes bg color and changes fonts  and fg color also

--- digital_clock.py
+++ digital_clock.py
@@ -7,21 +7,21 @@ root.title("Digital clock")
 root.config(bg="#FFBF00")
 
 def update_time():
-    current_time = time.strftime("%H:%M:%S")
-    current_date = time.strftime("%A, %d %B %Y %p")
+    current_time = time.strftime("%H:%M:%S %p")
+    current_date = time.strftime("%A, %d %B %Y")
 
     time_label.config(text=current_time)
     date_label.config(text=current_date)
 
     root.after(1000, update_time)
 
-time_label = Label(root, fg="#FAA291", font=("Arial", 60))
+time_label = Label(root,bg="#FFBF00", fg="#000000", font=("MS Gothic", 60))
 time_label.pack(pady=20)
 
-date_label = Label(root, fg="#A0A677", font=("Arial", 30))
+date_label = Label(root,bg="#FFBF00", fg="#000000", font=("MS Gothic", 30))
 date_label.pack(pady=20)
 
-button = Button(root, text="Quit", width=25, command=root.quit, bg="#45A8D9", fg="#F4FAE8")
+button = Button(root, text="Quit", width=25, command=root.quit, bg="#FFECD1", fg="#FF0000", font=("Arial", 15))
 button.pack()
 
 update_time()
