0e291e55529f6c98f87173e2325d8d966117a8e0
Utkarsh110-uz <utkarsh66488@gmail.com>
2025-08-30 23:37:33 +0530

    made major changes like removed feedback option and done changes in colors and add some new small things

--- personal_app.py
+++ personal_app.py
@@ -1,33 +1,31 @@
 from tkinter import *
 import tkinter as tk
 root = Tk()
-root.title("Calculator")
+root.title("Calculator app")
+root.config(bg="#D2C6F5")
 
-heading = Label(root, text= "Welcome to the Calculator", background="#98E1ED", foreground="black")
+heading = Label(root, text= "Welcome to EasyCount", font=("Arial Bold", 20),background="#D2C6F5", foreground="#2B024C")
 heading.pack(pady=10)
 
-button = tk.Button(root, text="Exit Program", width=60, command=root.destroy, background="#EB6565")
-button.pack(pady=10)
-
 frame = Frame(root)
-frame.pack(pady=5)
+frame.pack(pady=10)
 
-Label(frame, text="Number 1", foreground="#0C3EF2").grid(row=0, padx=5, pady=10)
-Label(frame, text="Number 2", foreground="#0C3EF2").grid(row=1, padx=5, pady=10)
+Label(frame, text="Number 1", foreground="black", font=("Arial", 15)).grid(row=0, padx=10, pady=10)
+Label(frame, text="Number 2", foreground="black", font=("Arial", 15)).grid(row=1, padx=10, pady=10)
 e1 = Entry(frame)
 e2 = Entry(frame)
-e1.grid(row=0, column=1, padx=5, pady=3)
-e2.grid(row=1, column=1, padx=5, pady=3)
+e1.grid(row=0, column=1, padx=10, pady=10)
+e2.grid(row=1, column=1, padx=10, pady=10)
 
-lb = Listbox(root)
-lb.insert(1, "Add = +")
-lb.insert(2, "Subtract = -")
-lb.insert(3, "Multiply = *")
-lb.insert(4, "Divide = /")
-lb.pack(pady=20)
+lb = Listbox(root, bg="#D2C6F5", fg="#1F0600", font=("Arial Bold", 15), borderwidth="0", highlightbackground="#D2C6F5", highlightcolor="#D2C6F5")
+lb.insert(1, "Add = (+)")
+lb.insert(2, "Subtract = (-)")
+lb.insert(3, "Multiply = (*)")
+lb.insert(4, "Divide = (/)")
+lb.pack(pady=10)
 
-result_label = Label(root, text="The result will be shown here", font=("Arial", 12), bg="#CED6E0")
-result_label.pack(pady=10)
+result_label = Label(root, text="The result will be shown here", font=("Arial", 15), bg="#D2C6F5")
+result_label.pack(pady=5)
 
 def calculate():
     try: 
@@ -36,7 +34,7 @@ def calculate():
 
         selected = lb.curselection()
         if not selected:
-            result_label.config(text="Please select the operation", bg="#CED6E0")
+            result_label.config(text="Please select the operation",font=("Arial", 15), bg="#D2C6F5")
 
         operation = lb.get(selected)
 
@@ -48,23 +46,22 @@ def calculate():
             result = num1 * num2
         elif("Divide" in operation):
             if(num2 == 0):
-                result_label.config(text="Zero division error", bg="#CED6E0")
+                result_label.config(text="Zero division error", font=("Arial, 15"), bg="#D2C6F5")
                 return
             result = num1 / num2
         else:
-            result_label.config(text="Unknown operation", bg="#8BF0CB")
+            result_label.config(text="Unknown operation", font=("Arial, 15"), bg="#D2C6F5")
             return
         
-        result_label.config(text = f"Result is: {result}", bg="#CED6E0")
+        result_label.config(text = f"Result is: {result}", font=("Arial, 15"), bg="#D2C6F5")
 
     except ValueError:
-        result_label.config(text="Please enter a valid number", bg="#CED6E0")
+        result_label.config(text="Please enter a valid number", font=("Arial", 15), bg="#D2C6F5")
 
-calculator_button = Button(root, text="Calculate", command=calculate, bg="#D5EDC2")
+calculator_button = Button(root, text="Calculate", command=calculate, bg="#D5EDC2", font=("Arial Bold", 12))
 calculator_button.pack(pady=10)
 
-feedback = Text(root, width=35, height= 4, bg="#F2ECE6")
-feedback.pack()
-feedback.insert(END, "Enter your feedback here:")
+button = tk.Button(root, text="Exit Program", width=60, command=root.destroy, background="#EB6565", font=("Arial Bold", 12))
+button.pack(pady=10)
 
 root.mainloop()
