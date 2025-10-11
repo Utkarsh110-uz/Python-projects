a0d36622e5b939f5bcd7e90f3036eff6305f3b53
Utkarsh110-uz <utkarsh66488@gmail.com>
2025-10-11 12:26:23 +0530

    Dice rolling app it's like a real dice shows number if you don't have a dice then you got this

---
+++ Dice_app.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import tkinter as tk
+import random
+
+root = Tk()
+root.title("Roll The Dice")
+root.config(bg="white")
+
+frame = Frame(root)
+frame.pack(pady=10)
+
+Label(frame, text="Enter yes to roll or no to quit", foreground="white", font=("Arial", 15)).grid(row=0, padx=10, pady=10)
+e1 = Entry(frame)
+e1.grid(row=0, column=1, padx=10, pady=10)
+
+result_label = Label(root, text="Number is", font=("Arial", 15), bg="#D2C6F5")
+result_label.pack(pady=5)
+
+def dice_roll():
+    user = e1.get()
+    if("yes" in user):
+        dice = random.randint(1, 6)
+        result_label.config(text=f"{dice}")
+    elif("no" in user):
+        result_label.config(text="Program ended")
+    else:
+        result_label.config(text="Please enter 'yes' or 'no'")
+
+Dice_roll_button = Button(root, text="Roll the dice", command=dice_roll, bg="#D5EDC2", font=("Arial Bold", 12))
+Dice_roll_button.pack(pady=10) 
+
+button = tk.Button(root, text="Exit", width=10, command=root.destroy, background="#EB6565", font=("Arial Bold", 12))
+button.pack(pady=10)
+
+root.mainloop()
