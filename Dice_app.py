6e156d0bb4fa3b508311398a1b6f735e71baab28
Utkarsh110-uz <utkarsh66488@gmail.com>
2025-10-12 17:39:45 +0530

    Done some minor changes

--- Dice_app.py
+++ Dice_app.py
@@ -9,7 +9,7 @@ root.config(bg="white")
 frame = Frame(root)
 frame.pack(pady=10)
 
-Label(frame, text="Enter yes to roll or no to quit", foreground="white", font=("Arial", 15)).grid(row=0, padx=10, pady=10)
+Label(frame, text="Enter yes to roll dice", foreground="white", font=("Arial", 15)).grid(row=0, padx=10, pady=10)
 e1 = Entry(frame)
 e1.grid(row=0, column=1, padx=10, pady=10)
 
@@ -21,13 +21,11 @@ def dice_roll():
     if("yes" in user):
         dice = random.randint(1, 6)
         result_label.config(text=f"{dice}")
-    elif("no" in user):
-        result_label.config(text="Program ended")
     else:
         result_label.config(text="Please enter 'yes' or 'no'")
 
 Dice_roll_button = Button(root, text="Roll the dice", command=dice_roll, bg="#D5EDC2", font=("Arial Bold", 12))
-Dice_roll_button.pack(pady=10) 
+Dice_roll_button.pack(pady=10)
 
 button = tk.Button(root, text="Exit", width=10, command=root.destroy, background="#EB6565", font=("Arial Bold", 12))
 button.pack(pady=10)
