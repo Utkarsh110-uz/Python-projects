12c6a8895eadee370467b491ba77f7b63974b6da
Utkarsh110-uz <utkarsh66488@gmail.com>
2025-12-20 15:34:31 +0530

    Updated code

--- Dice_app.py
+++ Dice_app.py
@@ -1,33 +1,28 @@
+# Importing modules
 from tkinter import *
-import tkinter as tk
 import random
 
-root = Tk()
-root.title("Roll The Dice")
-root.config(bg="white")
+# Initializing main window
+window = Tk()
 
-frame = Frame(root)
-frame.pack(pady=10)
+# Window related operations
+window.title("Roll The Dice")
+window.config(bg="lightcoral")
+window.geometry("200x150")
 
-Label(frame, text="Enter yes to roll dice", foreground="white", font=("Arial", 15)).grid(row=0, padx=10, pady=10)
-e1 = Entry(frame)
-e1.grid(row=0, column=1, padx=10, pady=10)
-
-result_label = Label(root, text="Number is", font=("Arial", 15), bg="#D2C6F5")
-result_label.pack(pady=5)
-
+# Initializing functions
 def dice_roll():
-    user = e1.get()
-    if("yes" in user):
-        dice = random.randint(1, 6)
-        result_label.config(text=f"{dice}")
-    else:
-        result_label.config(text="Please enter 'yes' or 'no'")
+    output_label.config(text=f"{random.randint(1, 6)}")
 
-Dice_roll_button = Button(root, text="Roll the dice", command=dice_roll, bg="#D5EDC2", font=("Arial Bold", 12))
-Dice_roll_button.pack(pady=10)
+# Initializing label
+output_label = Label(window, text="Number is", font=("Gill Sans", 15))
+output_label.pack(pady=5)
 
-button = tk.Button(root, text="Exit", width=10, command=root.destroy, background="#EB6565", font=("Arial Bold", 12))
-button.pack(pady=10)
+# Initializing buttons
+roll_dice = Button(window, text="Roll Dice", command=dice_roll)
+roll_dice.pack(pady=5)
 
-root.mainloop()
+exit_button = Button(window, text="Quit", command=window.destroy)
+exit_button.pack()
+
+window.mainloop()
