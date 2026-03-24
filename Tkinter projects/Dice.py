b8a77921ba23ba2ed3d33f1aaeecec41ce7d8ff2
Utkarsh110-uz <utkarsh66488@gmail.com>
2026-03-24 09:28:25 +0530

    Added styles to the GUI

--- Tkinter projects/Dice.py
+++ Tkinter projects/Dice.py
@@ -6,17 +6,23 @@ root = Tk()
 
 root.title("Rolling Dice")
 root.geometry("300x200")
+root.config(bg="#88798E")
 
 def roll():
     display_label.config(text=f"{random.randint(1, 6)}")
 
-display_label = Label(root, text="Number is")
+btn_style = ttk.Style()
+btn_style.theme_use("default")
+btn_style.configure("Dice.TButton", background="#c9567b", foreground="#ffffff", font=("Comic Sans MS", 12, "bold"), focuscolor="none")
+btn_style.map("Dice.TButton", background=[("active", "#c9567b")], foreground=[("active", "#ffffff")])
+
+display_label = Label(root, text="Number is", bg="#88798E", fg="#ff9ecd", font=("Comic Sans MS", 16, "bold"))
 display_label.pack(pady=5)
 
-roll_btn = ttk.Button(root, text="Roll The Dice", command=roll)
+roll_btn = ttk.Button(root, text="Roll The Dice", command=roll , style="Dice.TButton")
 roll_btn.pack(pady=5)
 
-quit_btn = ttk.Button(root, text="Quit", command=root.destroy, width=9)
+quit_btn = ttk.Button(root, text="Quit", command=root.destroy, width=9 , style="Dice.TButton")
 quit_btn.pack(pady=5)
 
 root.mainloop()
